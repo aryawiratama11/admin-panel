3f9c411bacd871cb8da54689edec0f62107b470c
elaraby <elsayed.fcis.1@gmail.com>
2021-03-31 22:05:16 +0200

    create admin/app.blade.php

---
+++ resources/views/layouts/admin/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--begin::Head-->
+
+<head>
+    <base href="">
+    <meta charset="utf-8" />
+    <title>Metronic | Dashboard</title>
+    <meta name="description" content="Updates and statistics" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+
+    <!--begin::Page Vendors Styles(used by this page)-->
+    <link href="{{ asset('assets/admin/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
+    <!--end::Page Vendors Styles-->
+
+
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link href="{{ asset('assets/admin/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/admin/plugins/custom/prismjs/prismjs.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets/admin/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <!--end::Global Theme Styles-->
+
+    <!--begin::Layout Themes(used by all pages)-->
+    <!--end::Layout Themes-->
+
+    <link rel="shortcut icon" href="{{ asset('assets/admin/media/logos/favicon.ico') }}" />
+
+</head>
+<!--end::Head-->
+
+<body>
+    <!--begin::Body-->
+
+    <body id="kt_body"
+        class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed page-loading">
+    </body>
+
+</body>
+
+</html>
